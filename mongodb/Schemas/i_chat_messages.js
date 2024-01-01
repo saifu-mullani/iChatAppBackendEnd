0faefda665b446a4e3449890554d2a8d6ad56b5a
Mohammedsaif Mullani <saifumullani@gmail.com>
2024-01-01 17:41:24 +0530

    chat message sequence fix

--- mongodb/Schemas/i_chat_messages.js
+++ mongodb/Schemas/i_chat_messages.js
@@ -9,12 +9,7 @@ const chatMessagesSchema = new Schema( {
     user2_id: {
         type: String,
     },
-    messages: {
-        message: String,
-        sender: String,
-        timestamp: Date,
-        messageId: { type: mongoose.Schema.Types.ObjectId, default:  () => new mongoose.Types.ObjectId()}
-      } 
+    messages: []
 })
 
 module.exports = model("i_chat_messages", chatMessagesSchema, "i_chat_messages");
