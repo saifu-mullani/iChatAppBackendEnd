077fc0efdfef6ce3518f36fa1806989f5a46233f
Mohammedsaif Mullani <saifumullani@gmail.com>
2024-01-01 15:37:55 +0530

    Build Deploy

---
+++ public/build/static/js/74.5cdee0a9.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkstudy_react=self.webpackChunkstudy_react||[]).push([[74],{74:(e,s,t)=>{t.r(s),t.d(s,{default:()=>u});const u={}}}]);
+//# sourceMappingURL=74.5cdee0a9.chunk.js.map
